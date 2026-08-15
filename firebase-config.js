1075299c1f8947f6302e1fac8779ed5665819479
Thanmay Rooba Nathan.S <155152263+thanmayroobanathans@users.noreply.github.com>
2026-08-15 16:04:28 +0530

    Change export to const for firebaseConfig

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyDQQMTFQ8W0mj0jGdvdeVxIB8kgc419OaU",
   authDomain: "vit-pulse.firebaseapp.com",
   projectId: "vit-pulse",
